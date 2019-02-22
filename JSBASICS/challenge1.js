2136c179238fcf42bdc320cd3fd8e864a6a6331a
Anjali George P <anjaligeorgep@gmail.com>
2019-02-22 19:36:33 +0530

    calculating BMI

---
+++ JSBASICS/challenge1.js
@@ -0,0 +1,9 @@
+var bmijohn,bmimark,heightm=170,heightj=165,weightm=45,weightj=55;
+
+bmijohn=weightj/(heightj*heightj);
+bmimark=weightm/(heightm*heightm);
+
+var ans=bmijohn>bmimark;
+console.log("Is mark's bmi higher than john's ? "+ans);
+
+
